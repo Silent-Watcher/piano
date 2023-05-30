34aeb46e9e1598fd52bf1db0d3e2202bf66f688a
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-05-30 12:08:08 +0430

    add sounds and click functionality

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,46 @@
 'use strict';
+const $$ = document;
+const pianoKeys = $$.querySelectorAll('.piano-keys .key');
+let audio = new Audio('audio/a.wav');
+const validKeys = [
+  'a',
+  'w',
+  's',
+  'e',
+  'd',
+  'f',
+  't',
+  'g',
+  'y',
+  'h',
+  'u',
+  'j',
+  'k',
+  'o',
+  'l',
+  'p',
+  ';',
+];
+
+function playTune(key) {
+  if (isCorrectKey(key)) {
+    let clickedKey = document.querySelector(`[data-key=${key}]`);
+    audio.src = `audio/${key}.wav`;
+    audio.play();
+    clickedKey.classList.add('active');
+    setTimeout(() => clickedKey.classList.remove('active'), 150);
+  }
+}
+
+function isCorrectKey(key) {
+  return validKeys.includes(key) ? true : false;
+}
+
+window.addEventListener('load', () => {
+  pianoKeys.forEach((key) => {
+    key.addEventListener('click', () => playTune(key.dataset.key));
+  });
+  $$.addEventListener('keydown', (event) => {
+    playTune(event.key);
+  });
+});
